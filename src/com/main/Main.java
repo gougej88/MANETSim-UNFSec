92ff6b782b447ddea88a7c7fc085554cc3cc8162
Jeff Gouge <gouge.jeff@gmail.com>
2014-10-01 21:49:08 -0400

    Updates for sparksee

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,10 +1,20 @@
 package com.main;
+import com.sparsity.sparksee.gdb.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws java.io.IOException, java.lang.Exception {
         // write your code here
-
+        SparkseeProperties.load("C:\\Users\\Jeff\\IdeaProjects\\MANETSim-UNFSec\\lib\\sparksee.cfg");
+        SparkseeConfig cfg = new SparkseeConfig();
+        Sparksee sparksee = new Sparksee(cfg);
+        Database db = sparksee.create("HelloSparksee.gdb", "HelloSparksee");
+        Session sess = db.newSession();
+        Graph graph = sess.getGraph();
+        // Use 'graph' to perform operations on the graph database
+        sess.close();
+        db.close();
+        sparksee.close();
 
         //Create graph grid of nodes
         Node a = new Node(1);
